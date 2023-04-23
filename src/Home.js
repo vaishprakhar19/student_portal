b8c8c3a30307fb1a371f1ecf0da7f373263bbfea
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-23 21:02:03 +0530

    asd

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Home.css';
-import {GiBookshelf} from 'react-icons/gi';
-import {MdOutlineFoodBank} from 'react-icons/md';
-import {GrDocumentTime} from 'react-icons/gr';
-import {BiTimer} from 'react-icons/bi';
-import {MdOutlineFestival} from 'react-icons/md';
-import {MdContactPhone} from 'react-icons/md';
+import { GiBookshelf } from 'react-icons/gi';
+import { MdOutlineFoodBank } from 'react-icons/md';
+import { BiTimer} from 'react-icons/bi';
+import { VscTable} from 'react-icons/vsc';
+import { MdOutlineFestival } from 'react-icons/md';
+import { MdContactPhone } from 'react-icons/md';
 
 export default function Home(props) {
   return (
     <div id="home">
       <div class="jumbotron">
         <div class="container intro">
-          <h1 class="display-3 main-heading"> Hello, {props.name.substring(0,props.name.indexOf(' '))}</h1>
+          <h1 class="display-3 main-heading"> Hello, {props.name.substring(0, props.name.indexOf(' '))}</h1>
           <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         </div>
       </div>
@@ -22,32 +22,32 @@ export default function Home(props) {
         <div class="row">
           <div class="col-md-4">
             <h2 className="sub-heading">Syllabus</h2>
-            <GiBookshelf className='icon'/>
+            <GiBookshelf className='icon' />
             <Link class="btn btn-secondary" to={'/syl' + props.course + props.year + props.stream} role="button">View Syllabus &raquo;</Link>
           </div>
           <div class="col-md-4">
             <h2 className="sub-heading">Time Table</h2>
-            <GrDocumentTime className='icon'/>
+            <VscTable className='icon' />
             <Link class="btn btn-secondary" to={'/tt' + props.course + props.year + props.stream} role="button">View Time Table &raquo;</Link>
           </div>
           <div class="col-md-4">
             <h2 className="sub-heading">Mess Menu</h2>
-            <MdOutlineFoodBank className='icon'/>
+            <MdOutlineFoodBank className='icon' />
             <Link class="btn btn-secondary" to='/messmenu' role="button">View Mess Menu &raquo;</Link>
           </div>
           <div class="col-md-4">
             <h2 className="sub-heading">Mess Timing</h2>
-            <BiTimer className='icon'/>
+            <BiTimer className='icon' />
             <Link class="btn btn-secondary" to='/messtime' role="button">View Mess Timing &raquo;</Link>
           </div>
           <div class="col-md-4">
             <h2 className="sub-heading">Holidays</h2>
-            <MdOutlineFestival className='icon'/>
+            <MdOutlineFestival className='icon' />
             <Link class="btn btn-secondary" to="/holidays" role="button">View Holidays &raquo;</Link>
           </div>
           <div class="col-md-4">
             <h2 className="sub-heading">Important Contacts</h2>
-            <MdContactPhone className='icon'/>
+            <MdContactPhone className='icon' />
             <Link class="btn btn-secondary" to="/contacts" role="button">View Contacts &raquo;</Link>
           </div>
         </div>
