8663f4aa0bec314e5a3281df43174a85d4ff7d91
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-23 20:20:39 +0530

    sfdsd

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,8 @@ export default function Home(props) {
   return (
     <div id="home">
       <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-3">Hello, {props.name.substring(0,props.name.indexOf(' '))}</h1>
+        <div class="container intro">
+          <h1 class="display-3 main-heading"> Hello, {props.name.substring(0,props.name.indexOf(' '))}</h1>
           <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         </div>
       </div>
