4b310511bf686cc24adad596d630f209556c91a9
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-23 16:45:17 +0530

    NMBM

--- src/components/MessTime.js
+++ src/components/MessTime.js
@@ -3,85 +3,97 @@ import React from 'react'
 export default function MessTime() {
   return (
     <div>
-<h1>MessTimeB</h1>
-<table className='table table-bordered'>
-    <tr>
-      <td></td>
-        <td><b/>BREAKFAST</td>
-        <td><b/>LUNCH</td>
-        <td><b/>TEA</td>
-        <td><b/>DINNER</td>
-       
-    </tr>
-  <tr>
-        <td>2ND YEAR</td>
-        <td>7:45 AM TO 8:10AM</td>
-        <td>12:30 PM TO 01:30PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:30 PM TO 7:50</td>
-        
-    </tr>
-  <tr>
-        <td>3RD YEAR BTECH +MCA 2 ND YEAR</td>
-        <td> 8:10AM TO 8:50AM</td>
-        <td>12:30 PM TO 01:30PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:50 PM TO 8:20 PM</td>
-        
-    </tr>
-  <tr>
-        <td>FINAL YEAR</td>
-        <td>8:00 AM TO 9:15AM</td>
-        <td>12:30 PM TO 01:30PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:30 PM TO 8:30 PM</td>
-        
-    </tr>
-    </table >
-<h1>MessTimeG</h1>
-<table className='table table-bordered'>
-    <tr>
-      <td></td>
-        <td><b/>BREAKFAST</td>
-        <td><b/>LUNCH</td>
-        <td><b/>TEA</td>
-        <td><b/>DINNER</td>
-       
-    </tr>
-  <tr>
-        <td>FIRST YEAR(B.TECH+MCA)</td>
-        <td>7:30 AM TO 7:50AM</td>
-        <td>12:30 PM TO 01:30PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:00 PM TO 7:25</td>
-        
-    </tr>
-  <tr>
-        <td>2ND YEAR BTECH </td>
-        <td> 7:50AM TO 8:10AM</td>
-        <td>12:35 PM TO 01:00PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:25 PM TO 7:40 PM</td>
-        
-    </tr>
-  <tr>
-        <td>3RD YEAR BTECH </td>
-        <td> 8:10AM TO 8:30AM</td>
-        <td>12:40 PM TO 01:15PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:40 PM TO 8:00 PM</td>
-        
-    </tr>
-  
-  <tr>
-        <td>FINAL YEAR(B.TECH+MCA)</td>
-        <td>8:30 AM TO 9:00AM</td>
-        <td>12:30 PM TO 01:15PM</td>
-        <td>4:00 PM TO 4:30 PM</td>
-        <td>7:45 PM TO 8:15 PM</td>
-        
-    </tr>
-    </table>
+      <h1>MessTimeB</h1>
+      <table className='table table-bordered'>
+        <tr>
+          <th></th>
+          <th>BREAKFAST</th>
+          <th>LUNCH</th>
+          <th>TEA</th>
+          <th>DINNER</th>
+
+        </tr>
+        <tr>
+          <th>1ST YEAR B.TECH + LE</th>
+          <td>7:30 AM TO 8:00 AM</td>
+          <td>12:30 PM TO 01:15 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:00 PM TO 7:40 PM</td>
+
+        </tr>
+        <tr>
+          <th>1ST YEAR MCA + LE</th>
+          <td>7:45 AM TO 8:10 AM</td>
+          <td>12:30 PM TO 01:15 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:20 PM TO 7:40 PM</td>
+
+        </tr>
+        <tr>
+          <th>2ND YEAR</th>
+          <td>7:45 AM TO 8:10 AM</td>
+          <td>12:30 PM TO 01:30 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:30 PM TO 7:50 PM</td>
+
+        </tr>
+        <tr>
+          <th>3RD YEAR BTECH +MCA 2ND YEAR</th>
+          <td> 8:10 AM TO 8:50 AM</td>
+          <td>12:30 PM TO 01:30 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:50 PM TO 8:20 PM</td>
+
+        </tr>
+        <tr>
+          <th>FINAL YEAR</th>
+          <td>8:00 AM TO 9:15 AM</td>
+          <td>12:30 PM TO 01:30 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:30 PM TO 8:30 PM</td>
+
+        </tr>
+      </table >
+      <h1>MessTimeG</h1>
+      <table className='table table-bordered'>
+        <tr>
+          <th></th>
+          <th>BREAKFAST</th>
+          <th>LUNCH</th>
+          <th>TEA</th>
+          <th>DINNER</th>
+        </tr>
+        <tr>
+          <th>FIRST YEAR(B.TECH+MCA)</th>
+          <td>7:30 AM TO 7:50 AM</td>
+          <td>12:30 PM TO 01:30 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:00 PM TO 7:25 PM</td>
+        </tr>
+        <tr>
+          <th>2ND YEAR BTECH </th>
+          <td>7:50 AM TO 8:10 AM</td>
+          <td>12:35 PM TO 01:00 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:25 PM TO 7:40 PM</td>
+        </tr>
+        <tr>
+          <th>3RD YEAR BTECH </th>
+          <td>8:10 AM TO 8:30 AM</td>
+          <td>12:40 PM TO 01:15 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:40 PM TO 8:00 PM</td>
+        </tr>
+
+        <tr>
+          <th>FINAL YEAR(B.TECH+MCA)</th>
+          <td>8:30 AM TO 9:00 AM</td>
+          <td>12:30 PM TO 01:15 PM</td>
+          <td>4:00 PM TO 4:30 PM</td>
+          <td>7:45 PM TO 8:15 PM</td>
+
+        </tr>
+      </table>
     </div>
   )
 }
