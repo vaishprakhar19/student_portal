2f79d0c7b24a8ce6694402c3b57628873a75c096
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-26 22:35:48 +0530

    dfgh

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,7 +3,29 @@ import React from 'react'
 export default function Contacts() {
   return (
     <div className="page">
-<h2>Contacts</h2> 
+<h2>Contacts</h2>
+<h1>CONTACTS</h1>
+  <h2>CHIEF HOSTEL WARDEN (BOYS)</h2>
+  <ul>
+    <li>1)DR.SANDESH TRIPATHI-94153 64502 </li>
+    <li>2)MR.NEERAJ SINGH BISHT-94151 71251 </li>
+    <li>3)MR.ABHAY KUMAR SHARMA-94111 99555</li>
+  </ul>
+  <h2>
+    CHIEF HOSTEL WARDEN (GIRLS)
+  </h2>
+  <ul>
+    <li>1)DR.SHILPI BISHT</li></ul>
+  <h2> ADDITIONAL CONTACTS
+  
+  </h2>
+  <ul>
+    <li>GATE CONTACT NUMBER-945811468
+    </li>
+    <li>REGISTRAR-+91 94129 96069</li>
+  </ul>
+  <h2>EMAIL ID</h2>
+  <ul><li>info@birlainstitute.co.in</li></ul> 
     </div>
   )
 }
