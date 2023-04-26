0b38910baaa43405f810c8cda55a526bc5f09046
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-26 23:57:03 +0530

    sad

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
 export default function Contacts() {
   return (
     <div className="page">
-      <h2>Contacts</h2>
-      <h5>CHIEF HOSTEL WARDEN (BOYS)</h5>
-      <ul>
-        <li>DR.SANDESH TRIPATHI-94153 64502 </li>
-        <li>MR.NEERAJ SINGH BISHT-94151 71251 </li>
-        <li>MR.ABHAY KUMAR SHARMA-94111 99555</li>
-      </ul>
-      <h5>
-        CHIEF HOSTEL WARDEN (GIRLS)
-      </h5>
-      <ul>
-        <li>DR.SHILPI BISHT</li>
-      </ul>
-      <h5> ADDITIONAL CONTACTS</h5>
-      <ul>
-        <li>GATE CONTACT NUMBER-945811468
-        </li>
-        <li>REGISTRAR-+91 94129 96069</li>
-      </ul>
-      <h5>EMAIL ID</h5>
-      <a>info@birlainstitute.co.in</a>
+
+
+
+      <div class="contact-card">
+        <div class="contact-card-details">
+          <p class="contact-text-title">Contacts</p>
+          <p class="contact-text-body">
+            <h5>CHIEF HOSTEL WARDEN (BOYS)</h5>
+            <ul>
+              <li>DR.SANDESH TRIPATHI: 94153 64502 </li>
+              <li>MR.NEERAJ SINGH BISHT: 94151 71251 </li>
+              <li>MR.ABHAY KUMAR SHARMA: 94111 99555</li>
+            </ul>
+            <h5>
+              CHIEF HOSTEL WARDEN (GIRLS)
+            </h5>
+            <ul>
+              <li>DR.SHILPI BISHT: 94581 73450</li></ul>
+            <h5> ADDITIONAL CONTACTS
+            </h5>
+            <ul>
+              <li>GATE CONTACT NUMBER: 945811468
+              </li>
+              <li>REGISTRAR: 94129 96069</li>
+            </ul>
+            <h5>EMAIL ID</h5>
+            <ul><li>info@birlainstitute.co.in</li></ul></p>
+        </div>
+      </div>
     </div>
   )
 }
