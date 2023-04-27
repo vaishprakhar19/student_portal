55acb7a0e55180c15ffac1c4f634074bd19c10cd
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-28 00:11:15 +0530

    dsa

--- src/About.js
+++ src/About.js
@@ -7,10 +7,10 @@ import { FaFacebook } from 'react-icons/fa';
 import './About.css'
 export function About() {
   return (
-    <div id='about' className='page page-padding'>
+    <div id='about' className='page'>
       <div>
         <div className='about-intro'>
-          <h2>About Us</h2>
+          <h1>About Us</h1>
           <p>A student portal is a feature that every educational institute needs to have in the digital age. The most important benefit of a student portal is that it is time and cost-effective for the students as well as the University. It saves time and money for the students as all they have to do is access the student portal for whatever information they require. Secondly, students in university usually have a lot of things that they have to keep track of, such as mess schedules, timetables etc. This is even more so the case when a student is just joining an educational institute. A student portal makes it easy for a student to keep track of all the schedules and timetables and any changes to the same. it helps students stay on top of their deadlines. It helps to streamline processes.</p>
         </div>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1583.6669095201187!2d79.55434014805883!3d29.35645515673876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39a099dbdc0befa7%3A0xa08407c1dac1f3ac!2sBirla%20Institute%20of%20Applied%20Sciences%2C%20Bhimtal-263136%20(Uttarakhand)!5e0!3m2!1sen!2sin!4v1681580326265!5m2!1sen!2sin" width="500" height="400" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="Map"></iframe>
@@ -25,6 +25,7 @@ export function About() {
           <div className='about-icon'><a href="https://mail.google.com/mail/" style={{ display: 'inline-block' }} rel='noreferrer'><FaMailBulk></FaMailBulk></a></div>
         </div>
       </div>
+      <div className='footer'>&copy; 2023 BIAS | Developed by Prakhar Vaish | Aarushi Pandey | Radhika Agarwal</div>
     </div>
   )
 }
