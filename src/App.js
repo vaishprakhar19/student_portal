815c42a9e9fb4907b175b6bc331966f335a1f738
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-28 04:54:24 +0530

    sddf

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
   const [year, setYear] = useState('');
   const [stream, setStream] = useState('');
   const [login, setLogin] = useState(false);
-  const [daysch, setDaysch] = useState(false);
   useEffect(() => {
     AOS.init();
     AOS.refresh();
